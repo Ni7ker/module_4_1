4d4ab8fb607d0bbfdd8ac7de06a8ab88695a831d
Ni7ker <rustyamka210@gmail.com>
2024-12-14 20:33:54 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,15 @@
+# module_4_1.py
+from fake_math import divide as fake_divide
+from true_math import divide as true_divide
+
+# Вызов функций
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+
+# Вызов результата
+print(result1)
+print(result2)
+print(result3)
+print(result4)
